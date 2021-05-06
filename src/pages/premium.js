74f8e183417cc1bf72c2a27506e49a2820ca068c
okayyyy <zghan2005@gmail.com>
2021-05-06 16:27:53 -0700

    Added premium page and updated embed for site

---
+++ src/pages/premium.js
@@ -0,0 +1,17 @@
+import { Helmet } from 'react-helmet';
+import '../styles/premium.css'
+
+function Premium() {
+
+    return (
+        <div id='premium-body' className='body'>
+
+            <Helmet>
+                <title>Voice Rooms • Premium</title>
+            </Helmet>
+
+        </div>
+    )
+}
+
+export default Premium;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import parseRoutes from './utils/parser.js'
 import Home from './pages/home.js'
 import Commands from './pages/commands.js'
+import Premium from './pages/premium.js'
 import Help from './pages/help.js'
 
 const routes = [
@@ -11,6 +12,10 @@ const routes = [
     {
         path: '/commands',
         component: Commands
+    },
+    {
+        path: '/premium',
+        component: Premium
     },
     {
         path: '/help',
