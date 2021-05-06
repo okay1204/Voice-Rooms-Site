95b90f432cefdf2f9057f8db3e85c4dc4156da14
okayyyy <zghan2005@gmail.com>
2021-05-06 16:40:50 -0700

    Added premium page

--- src/pages/premium.js
+++ src/pages/premium.js
@@ -10,6 +10,16 @@ function Premium() {
                 <title>Voice Rooms • Premium</title>
             </Helmet>
 
+            <h1 className='premium-title'>Premium</h1>
+
+            <p className='premium-desc'>
+                Premium is on the way, with new big features planned!
+                The price is going to be pretty cheap, most likely a one time payment of $5.
+                And no, don't worry, no features from the existing free version will be restricted or taken away. New features only!
+                <br /> <br />
+                Be sure to check out this page once premium has released!
+            </p>
+
         </div>
     )
 }
