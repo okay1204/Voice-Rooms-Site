f675d20e005a5d73bb7190b1ce8e39838e1f7d4f
okayyyy <zghan2005@gmail.com>
2021-12-04 19:17:45 -0800

    Add digitalocean and paypal promos

---
+++ src/components/digitalOcean.js
@@ -0,0 +1,9 @@
+function DigitalOcean() {
+    return (
+        <a href="https://www.digitalocean.com/?refcode=199b47f53d9b&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge">
+            <img src="https://web-platforms.sfo2.cdn.digitaloceanspaces.com/WWW/Badge%201.svg" alt="DigitalOcean Referral Badge" />
+        </a>
+    )
+}
+
+export default DigitalOcean
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,20 @@
 import '../styles/footer.css'
 
 import WordLogo from '../images/word logo.png'
+import DigitalOcean from './digitalOcean.js';
+import PayPal from './payPal.js';
 
 function Footer() {
     return (
         <footer className='footer'>
-            <a href="/">
+            <a className='footer-logo-anchor' href="/">
                 <img id="footer-logo" src={WordLogo} alt="Voice Rooms logo" />
             </a>
+
+            <div className='footer-promos'>
+                <DigitalOcean />
+                <PayPal />
+            </div>
         </footer>
     )
 }
